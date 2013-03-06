b2092666992b9e1e3599d672017f41eb2cf07504
illnino ho <illnino.giggs@gmail.com>
2013-03-07 00:15:41 +0800

    devise cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visa::Application.config.secret_token = '77d2a8f2a35f2080444f4598108cfe42e13e652c73f0fce485e99d9f056c7a4781b8635dbf79158f49cc25fc34e621ebfcd77a77364c815dd0d1737b773380da'
